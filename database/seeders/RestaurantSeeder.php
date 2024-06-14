c51babc1993992e998d4ba0cf97ab64c5bbf5479
GioMench <giorgiaglm@gmail.com>
2024-06-14 11:04:58 +0200

    add seeder for users and restaurants

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -9,15 +9,24 @@ use App\Models\Restaurant;
 class RestaurantSeeder extends Seeder
 {
 
-
-
-
-
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
+        $restaurants = config('restaurants_db.restaurants');
+        foreach ($restaurants as $restaurant ) {
+           $newRestaurant = new Restaurant();
+           $newRestaurant->address=$restaurant['address'];
+           $newRestaurant->mail=$restaurant['mail'];
+           $newRestaurant->phone_number=$restaurant['phone_number'];
+           $newRestaurant->vat=$restaurant['vat'];
+           $newRestaurant->name=$restaurant['name'];
+           $newRestaurant->image=$restaurant['image'];
+           $newRestaurant->user_id=$restaurant['user_id'];
+           $newRestaurant->save();
+           
+        }
         
     }
 }
