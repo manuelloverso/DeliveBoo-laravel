3b3ceaf3366136416439e5e034fbc5628034ac13
simoneevacca <v.simone.vacca@gmail.com>
2024-06-14 15:42:36 +0200

    add edit button to plates card

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -5,6 +5,7 @@
 
         @foreach ($plates as $plate)
             <h1>{{ $plate->name }}</h1>
+            <a class="btn btn-secondary" href="{{ route('admin.plates.edit', $plate) }}">Modifica il piatto</a>
         @endforeach
     </div>
 @endsection
