10740cec0517468a97f5915b5f754943358c1c57
Manuel <manuelloverso60@gmail.com>
2024-06-19 16:11:35 +0200

    minor fix

--- database/migrations/2024_06_13_125934_create_restaurants_table.php
+++ database/migrations/2024_06_13_125934_create_restaurants_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,11 +13,11 @@ return new class extends Migration {
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
+            $table->string('restaurant_name', 50);
             $table->string('address', 100);
             $table->string('restaurant_email', 100)->unique();
             $table->string('phone_number', 15)->nullable();
             $table->char('p_iva', 11);
-            $table->string('restaurant_name', 50);
             $table->string('image', 255)->nullable();
             $table->timestamps();
         });
