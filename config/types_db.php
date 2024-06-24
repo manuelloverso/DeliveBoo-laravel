6c309a9e3b3c515ac2840946c669c2909c4feee8
Manuel <manuelloverso60@gmail.com>
2024-06-24 09:26:01 +0200

    add images for types

--- config/types_db.php
+++ config/types_db.php
@@ -4,43 +4,43 @@ return [
     'types' => [
         [ //1
             'name' => 'Internazionale',
-            'image' => ''
+            'image' => 'https://www.gdoweek.it/wp-content/uploads/sites/7/2024/01/piatti-etnici.jpg'
         ],
         [ //2
             'name' => 'Italiano',
-            'image' => ''
+            'image' => 'https://www.ricettasprint.it/wp-content/uploads/2024/04/Spaghetti.jpg'
         ],
         [ //3
             'name' => 'Cinese',
-            'image' => ''
+            'image' => 'https://www.giallozafferano.it/images/ricette/230/23009/foto_hd/hd450x300.jpg'
         ],
         [ //4
             'name' => 'Sushi',
-            'image' => ''
+            'image' => 'https://sweetest.it/wp-content/uploads/2021/06/sushi-time.jpg'
         ],
         [ //5
             'name' => 'Indiano',
-            'image' => ''
+            'image' => 'http://blog.bauer.it/wp-content/uploads/2022/01/piatti-indiani-e1640085754987.jpg'
         ],
         [ //6
             'name' => 'Messicano',
-            'image' => ''
+            'image' => 'https://www.citizenpost.it/wp-content/uploads/2020/01/cucina-messicana.jpg'
         ],
         [ //7
             'name' => 'Fast Food',
-            'image' => ''
+            'image' => 'https://i0.wp.com/societefacile.com/wp-content/uploads/2024/01/Tout-savoir-sur-le-Marche-des-fast-food.webp?fit=1024%2C591&ssl=1'
         ],
         [ //8
             'name' => 'Pizzeria',
-            'image' => ''
+            'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVJiMGt-SI69kdqasJ_evpDrAnV_NmCvgkMw&s'
         ],
         [ //9
             'name' => 'Pesce',
-            'image' => ''
+            'image' => 'https://www.ecobiorevolution.it/wp-content/uploads/2018/02/mangiare-pesce.jpg'
         ],
         [ //10
             'name' => 'Carne',
-            'image' => ''
+            'image' => 'https://countryhouseperbacco.it/public/Files/rif000002/69/carne-alla-brace.jpg'
         ],
     ]
 ];
