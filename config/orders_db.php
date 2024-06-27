f2997f306eaf37af195c919d171c185a0331cb57
mattemutti <mattemutti@gmail.com>
2024-06-27 12:24:36 +0200

    update order seeder

--- config/orders_db.php
+++ config/orders_db.php
@@ -92,7 +92,274 @@ return [
             "status" => "fallito",
             "total" => "110.00",
             "date" => "2024-3-12"
+        ],
 
+
+        //dddddddfòdflmgèkfjgè+kpfngèwdkfjgnwpdfkng+wpdfkng+wpkdfng+wpk
+
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Giacomo",
+            "customer_lastname" => "Borsa",
+            "customer_address" => 'via giuggiola 12',
+            "customer_phone" => "1234565590",
+            "customer_email" => "giagiug@gmail.com",
+            "status" => "spedito",
+            "total" => "59.00",
+            "date" => "2024-1-12"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Giovanni",
+            "customer_lastname" => "Biondo",
+            "customer_address" => 'via girolamo 12',
+            "customer_phone" => "1234565555",
+            "customer_email" => "giobio@gmail.com",
+            "status" => "spedito",
+            "total" => "74.00",
+            "date" => "2024-1-19"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Maurizio",
+            "customer_lastname" => "zaccheroni",
+            "customer_address" => 'via papilloma 51',
+            "customer_phone" => "1234565852",
+            "customer_email" => "mauri@gmail.com",
+            "status" => "spedito",
+            "total" => "42.00",
+            "date" => "2024-1-25"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Francesco",
+            "customer_lastname" => "Muffa",
+            "customer_address" => 'via fungo 8',
+            "customer_phone" => "1234565858",
+            "customer_email" => "fungo8@gmail.com",
+            "status" => "spedito",
+            "total" => "25.00",
+            "date" => "2024-2-25"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Paolo",
+            "customer_lastname" => "Paolissimo",
+            "customer_address" => 'via paolissimissimo 10',
+            "customer_phone" => "1234565874",
+            "customer_email" => "paolissimo@gmail.com",
+            "status" => "spedito",
+            "total" => "95.00",
+            "date" => "2024-2-2"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Nicola",
+            "customer_lastname" => "Merda",
+            "customer_address" => 'via infame 999',
+            "customer_phone" => "1234565874",
+            "customer_email" => "infame@gmail.com",
+            "status" => "spedito",
+            "total" => "99.00",
+            "date" => "2024-2-7"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Nicoletta",
+            "customer_lastname" => "Piede",
+            "customer_address" => 'via oroscopo 9',
+            "customer_phone" => "1234565874",
+            "customer_email" => "piede9@gmail.com",
+            "status" => "spedito",
+            "total" => "9.00",
+            "date" => "2024-3-21"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Aldo",
+            "customer_lastname" => "Muschio",
+            "customer_address" => 'via piacere 45',
+            "customer_phone" => "1234565845",
+            "customer_email" => "uschio@gmail.com",
+            "status" => "spedito",
+            "total" => "54.00",
+            "date" => "2024-3-22"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Giulia",
+            "customer_lastname" => "Muzzi",
+            "customer_address" => 'via piacere 1',
+            "customer_phone" => "1234565789",
+            "customer_email" => "giumuz@gmail.com",
+            "status" => "spedito",
+            "total" => "74.00",
+            "date" => "2024-3-15"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Andrea",
+            "customer_lastname" => "Anto",
+            "customer_address" => 'via sedesina 5',
+            "customer_phone" => "1234565789",
+            "customer_email" => "tal@gmail.com",
+            "status" => "spedito",
+            "total" => "10.00",
+            "date" => "2024-4-8"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Pesce",
+            "customer_lastname" => "aprile",
+            "customer_address" => 'via squalo 7',
+            "customer_phone" => "1234565789",
+            "customer_email" => "pescapr@gmail.com",
+            "status" => "spedito",
+            "total" => "82.00",
+            "date" => "2024-4-1"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Ruby",
+            "customer_lastname" => "Rose",
+            "customer_address" => 'via dj 5',
+            "customer_phone" => "1234565733",
+            "customer_email" => "rubyrose@gmail.com",
+            "status" => "spedito",
+            "total" => "23.00",
+            "date" => "2024-4-18"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Mario",
+            "customer_lastname" => "Zanna",
+            "customer_address" => 'via diocesi 5',
+            "customer_phone" => "1234565733",
+            "customer_email" => "marzan@gmail.com",
+            "status" => "spedito",
+            "total" => "33.00",
+            "date" => "2024-5-28"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Zendaya",
+            "customer_lastname" => "Bella",
+            "customer_address" => 'via america 5',
+            "customer_phone" => "1234565733",
+            "customer_email" => "zenbel@gmail.com",
+            "status" => "spedito",
+            "total" => "45.00",
+            "date" => "2024-5-29"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Brad",
+            "customer_lastname" => "Pitt",
+            "customer_address" => 'via Hollywood 584',
+            "customer_phone" => "1234565584",
+            "customer_email" => "brutpitt@gmail.com",
+            "status" => "spedito",
+            "total" => "64.00",
+            "date" => "2024-5-8"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Riccardo",
+            "customer_lastname" => "Reynolds",
+            "customer_address" => 'via Hollywood 54',
+            "customer_phone" => "1234565584",
+            "customer_email" => "raynold@gmail.com",
+            "status" => "spedito",
+            "total" => "35.00",
+            "date" => "2024-6-10"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Alberto",
+            "customer_lastname" => "Elenco",
+            "customer_address" => 'via sci 85',
+            "customer_phone" => "3334565584",
+            "customer_email" => "rscialb@gmail.com",
+            "status" => "spedito",
+            "total" => "67.00",
+            "date" => "2024-6-15"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Saverio",
+            "customer_lastname" => "Sempreserio",
+            "customer_address" => 'via maschio 8',
+            "customer_phone" => "3334565582",
+            "customer_email" => "savemash@gmail.com",
+            "status" => "spedito",
+            "total" => "29.00",
+            "date" => "2024-6-25"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Elisa",
+            "customer_lastname" => "Corvaglia",
+            "customer_address" => 'via max 8',
+            "customer_phone" => "3334565582",
+            "customer_email" => "elicor@gmail.com",
+            "status" => "spedito",
+            "total" => "69.00",
+            "date" => "2024-7-25"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Elisabetta",
+            "customer_lastname" => "Canalis",
+            "customer_address" => 'via milano 77',
+            "customer_phone" => "3334565569",
+            "customer_email" => "elican@gmail.com",
+            "status" => "spedito",
+            "total" => "90.00",
+            "date" => "2024-7-2"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Ivana",
+            "customer_lastname" => "Chiappa",
+            "customer_address" => 'via 666 99',
+            "customer_phone" => "3334565999",
+            "customer_email" => "chiappa@gmail.com",
+            "status" => "spedito",
+            "total" => "28.00",
+            "date" => "2024-7-17"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Blanco",
+            "customer_lastname" => "Giovane",
+            "customer_address" => 'via calvages 9',
+            "customer_phone" => "3334565636",
+            "customer_email" => "blanco@gmail.com",
+            "status" => "spedito",
+            "total" => "85.00",
+            "date" => "2024-8-1"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Fausto",
+            "customer_lastname" => "Coma",
+            "customer_address" => 'via salò 85',
+            "customer_phone" => "3334565366",
+            "customer_email" => "cose@gmail.com",
+            "status" => "spedito",
+            "total" => "52.00",
+            "date" => "2024-8-18"
+        ],
+        [
+            "restaurant_id" => "1",
+            "customer_name" => "Stefano",
+            "customer_lastname" => "Bamba",
+            "customer_address" => 'via ligure 3',
+            "customer_phone" => "3334565385",
+            "customer_email" => "ligu@gmail.com",
+            "status" => "spedito",
+            "total" => "159.00",
+            "date" => "2024-8-25"
         ],
     ]
 ];
