c0330ead9a14fa81c831a630659e460bd11c3027
mattemutti <mattemutti@gmail.com>
2024-06-28 17:51:22 +0200

    update dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,11 +10,42 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $user = auth()->user();
+        $restaurant = $user->restaurant;
+        $orders = $restaurant->orders->sortByDesc('id');
+        $plates = $restaurant->plates;
+        $plateData = [
+            'labels' => [],
+            'data' => []
+        ];
+        //dd($plate);
+
+
+        // Grafico ordini per mese
+        $data = [
+            'labels' => [],
+            'data' => []
+        ];
+        $date = date_create('now');
+
+        for ($i = 0; $i < 12; $i++) {
+            // var_dump(date_format($date, 'Y-m-d h:m:s') . "\n");
+
+            $count = 0;
+            foreach ($orders as $order) {
+                if ($order->created_at->format('m-y') == $date->format('m-y')) {
+                    $count = $count + 1;
+                }
+            }
+            array_unshift($data['labels'], $date->format('m-y'));
+            array_unshift($data['data'], $count);
+            date_sub($date, date_interval_create_from_date_string('1 month'));
+        }
 
         $types = Type::all();
         $user = auth()->user();
         if ($user->restaurant) {
-            return view('admin.dashboard', compact('user'));
+            return view('admin.dashboard', compact('user', 'data', 'plates', 'plateData'));
         } else {
             return view('admin.restaurants.create', compact('types'));
         }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,110 +1,119 @@
-@extends('layouts.appDashboard')
+@extends('layouts.app')
 
 @section('content')
-    <div class="dashboard">
-        <div class=" container-fluid py-5">
-            <div class="row gap-5 ">
-                @include('partials.message-error')
+    <div class="container">
+        <div class="row py-4 justify-content-center">
 
-                <!--info-restaurant-->
-                <div class=" card col-12 col-sm-6 col-md-6 col-lg-12 text-body-secondary mx-auto">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            @if (Str::startsWith($user->restaurant->image, 'https://'))
-                                <img loading="lazy" class="h-100 w-100 img-fluid rounded-start "
-                                    src="{{ $user->restaurant->image }}" alt="">
-                            @else
-                                <img loading="lazy" class="h-100 w-100 img-fluid rounded-start "
-                                    src="{{ asset('storage/' . $user->restaurant->image) }}" alt="">
-                            @endif
+            <div class="d-flex gap-4 pt-4 resp_card">
+
+                <div class="card col-6 ">
+                    <div class="text-center my-4">
+                        <h1>Benvenuto Nome risto</h1>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+                <div class="card col-6 ">
+
+                    <div class="card-body">
+                        Dati Ristorante
+
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div class="d-flex gap-4 pt-4 resp_card">
+
+                <div class="card col-8">
+                    <div class="card-title">
+                        <div class="text-center my-4">
+                            <h2>Statistiche vendite</h2>
                         </div>
-                        <div class="col-md-8">
-                            <div class="card-body px-4">
-                                <div>
-
-                                    <h1>{{ $user->restaurant->restaurant_name }}</h1>
-                                    <div class="d-flex gap-2">
-                                        @foreach ($user->restaurant->types as $type)
-                                            <div class="bg_orange-dark px-2 rounded text-white">{{ $type->name }}</div>
-                                        @endforeach
-                                    </div>
-                                    <div class="py-2">
-                                        <i class="fa-solid fa-location-dot"></i> {{ $user->restaurant->address }}
-                                    </div>
-                                    <div class="py-2">
-                                        <i class="fa-solid fa-phone"></i> {{ $user->restaurant->phone_number }}
-                                    </div>
-                                    <div class="py-2">
-                                        <strong>PI:</strong> {{ $user->restaurant->p_iva }}
-                                    </div>
-
-                                </div>
-                            </div>
+                    </div>
+                    <div class="card-body">
+                        <h2 class="text-center">Ordini degli ultimi 12 mesi</h2>
+                        <div class="mb-4" style="width: 80%; margin: auto;">
+                            <canvas id="barChart"></canvas>
                         </div>
                     </div>
                 </div>
 
-                <!--statistic-->
-                <div class="card grafic-card col-12 col-sm-4 col-md-6 col-lg-4 text-body-secondary mx-auto">
-                    <img src="{{ Vite::asset('resources/img/grafico.jpg') }}" class="card-img-top" alt="statistic grafic">
-                    <div class="card-body p-0">
-                        <a class="text-decoration-none btn text-white w-100 rounded-0 text-uppercase fs-5"
-                            href="{{ route('admin.barchart.index') }}">
-                            Le tue statistiche
-                        </a>
-                        <p class="card-text px-5 py-2 text-center">
-                            Ti aiutiamo a tenere sempre sotto controllo il rendimento del tuo ristorante
-                        </p>
+                <div class="card col-4">
+                    <div class="text-center my-4">
+                        <h2>Grafico del totale degli ordini per mese</h2>
+                    </div>
+                    <div class="card-body text-center">
+                        Da Fare
+
+                    </div>
+
+
+
+                </div>
+            </div>
+            <div class="d-flex gap-4 pt-4 resp_card">
+
+                <div class="card col-6">
+                    <div class="card-title">
+
+                        <div class="text-center my-4">
+                            <h2>Il tuo Menù</h2>
+                        </div>
+                    </div>
+                    <div class="card-body ">
+                        Gestisci i tuoi piatti
+
                     </div>
                 </div>
 
-                <!--menu-->
-                <div class="card grafic-card col-12 col-sm-3 col-md-6 col-lg-3 text-body-secondary mx-auto">
-                    <img src="{{ Vite::asset('resources/img/piatti.png') }}" class="card-img-top" alt="statistic grafic">
-                    <div class="card-body p-0">
-                       
-                        <p class="card-text px-5 py-2 text-center ">
-                            Aggiungi, modifica o elimina i piatti del tuo menù<br>
-                            Quando vuoi e come vuoi!
-                        </p>
-                         
+                <div class="card col-6 ">
+                    <div class="card-title">
+
+                        <div class="text-center my-4">
+                            <h2>I Tuoi Ordini</h2>
+                        </div>
                     </div>
-                    <a class="text-decoration-none btn text-white w-100 rounded-0 text-uppercase fs-5 rounded-bottom"
-                            href="{{ route('admin.plates.index') }}">
-                           Il tuo menù 
-                        </a>
-                </div>
+                    <div class="card-body">
+                        Gestisci i tuoi Ordini
 
-                <!--info-orders-->
-                <div class="card grafic-card col-12 col-sm-5 col-md-6 col-lg-4 text-body-secondary mx-auto">
-                    <img src="{{ Vite::asset('resources/img/deliverome.png') }}" class="img-fluid rounded-top"
-                        alt="delivery">
-                    <div class="card-body p-0">
-                        <a class="text-decoration-none btn text-white w-100 rounded-0 text-uppercase fs-5"
-                            href="{{ route('admin.orders.index') }}">
-                            Ordini ricevuti
-                        </a>
-                        <p class="card-text px-5 py-2 text-center">
-                            Il tuo storico dal giorno zero <br>
-                            Con noi nessun ordine andrá perso!
-
-                        </p>
                     </div>
                 </div>
             </div>
 
 
-            {{-- @if ($user->restaurant) --}}
-
-
         </div>
+    </div>
 
-        {{-- @else
-                <a class="btn btn-primary" href="{{ route('admin.restaurants.create') }}">
-                    Crea il tuo ristorante!
-                </a>
-            @endif --}}
+
+
+
+
 
     </div>
-    </div>
+    <script>
+        var ctx = document.getElementById('barChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: @json($data['labels']),
+                datasets: [{
+                    label: 'N° Ordini',
+                    data: @json($data['data']),
+                    backgroundColor: 'rgba(255, 220, 0, 0.5)',
+                    borderColor: 'rgba(255, 220, 0, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
 @endsection
