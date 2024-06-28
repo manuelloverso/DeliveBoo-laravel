14955642360e0e074b2b492fca67c8e380b41ec0
GioMench <giorgiaglm@gmail.com>
2024-06-28 16:47:51 +0200

    fix style show order

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -7,62 +7,106 @@ $plateList = json_decode($plates);
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <a class="btn mt-4" href="{{ route('admin.orders.index') }}"><i class="fa fa-arrow-circle-left"
+        <a class="btn mt-4 mb-2" href="{{ route('admin.orders.index') }}"><i class="fa fa-arrow-circle-left"
                 aria-hidden="true"></i></a>
 
-                
-        <h1 class="py-4">Dettagli ordine</h1>
-        <h5 class="text-secondary pb-4">Ordine n°{{ $order->id }} del {{ $order->created_at }}</h5>
+        <div class="card p-4">
+            <div class="card-title">
+                <h1 class="py-4">Dettagli ordine</h1>
+            </div>
+            <div class="card-body">
+                <h5 class="text-secondary pb-4">Ordine n°{{ $order->id }} del {{ $order->created_at }}</h5>
 
-        <div class="row row-cols-1 row-cols-md-2 ">
-            <div class="col">
-                <h3>Lista ordine</h3>
-                <div class="card border-0">
-                    <div class="table-responsive">
-                        <table class="table table-secondary">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Piatto</th>
-                                    <th scope="col">Quantità</th>
-                                    <th scope="col">Prezzo</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($plateList as $plate)
-                                    <tr class="">
-                                        <td scope="row">{{ $plate->name }}</td>
-                                        <td>{{ $plate->qty }}</td>
-                                        <td>{{ $plate->price }}€</td>
-                                    </tr>
-                                @endforeach
+                <div class="row row-cols-1 row-cols-md-2 ">
+                    <div class="col">
+
+                        <div class="card text-center">
+                            <div class="card-header">
+                                <h3>Lista ordine</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Piatto</th>
+                                                <th scope="col">Quantità</th>
+                                                <th scope="col">Prezzo</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($plateList as $plate)
+                                                <tr class="">
+                                                    <td scope="row">{{ $plate->name }}</td>
+                                                    <td>{{ $plate->qty }}</td>
+                                                    <td>{{ $plate->price }}€</td>
+                                                </tr>
+                                            @endforeach
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="card-footer text-body-secondary">
                                 <tr class="">
                                     <td class="text-center" colspan="3" scope="row"><strong>Totale:
                                         </strong>{{ $order->total }}€</td>
                                 </tr>
-                            </tbody>
-                        </table>
+                            </div>
+                        </div>
+
+
+
                     </div>
+                    <div class="col">
 
 
+                        <div class="card text-center">
+                            <div class="card-header">
+                                <h3>Informazioni cliente</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Nome</th>
+                                                <th scope="col">Cognome</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Telefono</th>
+                                                
+
+
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                          
+                                                <tr class="">
+                                                    <td scope="row">{{ $order->customer_name }}</td>
+                                                    <td>{{ $order->customer_lastname }}</td>
+                                                    <td>{{ $order->customer_email }}</td>
+                                                    <td>{{ $order->customer_phone }}</td>
+                                                    
+
+                                                </tr>
+                                            
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="card-footer text-body-secondary">
+                                <tr >
+                                   <td><strong>Indirizzo di consegna: </strong>{{ $order->customer_address }}</td> 
+                                </tr>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-
             </div>
-            <div class="col">
-                <h3>Informazioni cliente</h3>
-
-                <div class="card w-50">
-                    <div class="m-1"><strong>Nome: </strong>{{ $order->customer_name }}</div>
-                    <div class="m-1"><strong>Cognome: </strong>{{ $order->customer_lastname }}</div>
-                    <div class="m-1"><strong>Email: </strong>{{ $order->customer_email }}</div>
-                    <div class="m-1"><strong>Telefono: </strong>{{ $order->customer_phone }}</div>
-                    <div class="m-1"><strong>Indirizzo: </strong>{{ $order->customer_address }}</div>
 
 
-                </div>
-
-            </div>
         </div>
-
-
     </div>
 @endsection
