9d90a57af08a58ffcde178d473ac51cb0fea7d96
Manuel <manuelloverso60@gmail.com>
2024-07-01 10:48:46 +0200

    minor fix

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -86,7 +86,7 @@ class OrderController extends Controller
                     'order_id' => $newOrder->id,
                     'order_total' => $request->total,
                     'customer_name' => $request->customer_name,
-                    'customer_lastname' => $request->customer_name,
+                    'customer_lastname' => $request->customer_lastname,
                     'customer_email' => $request->customer_email,
                     'customer_phone' => $request->customer_phone,
                     'customer_address' => $request->customer_address,
